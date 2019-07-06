e39a6881c5ec28ba7bbf20e9604c5cd4bb109967
roger gc <nppcpp@hotmail.com>
2019-07-06 12:47:43 +0200

    env

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,5 @@
 import express from 'express'
-
 const app = express()
 const port = process.env.PORT||4000
-
-app.use(express.static(process.env.OUTPUT_FOLDER))
-
+app.use(express.static('dist'))
 app.listen(port,()=>console.log(`Now browse to localhost:${port}`))

58ca99d9d09310fa77358f236f7e3dd7769a0e36
roger gc <nppcpp@hotmail.com>
2019-07-06 12:23:37 +0200

    stage login

--- src/comps/content/index.jsx
+++ src/comps/content/index.jsx
@@ -20,31 +20,40 @@ export default
     setLogin:setLogin1
   }
   const el=
-  <div className={`${style.border} ${style.height}`}>
-    <div className={`${style.flex1} ${style.height}`}>
-      <div className={style.flex2}>
-        <Route path='/about' render=
-        {
-          ()=>
+  <div>
+    {
+      login1.loggedin?
+      <div className={style.flex0}>
+        <div className={`${style.absolute} ${style2.border2}`}
+          style={{top:'76px',right:'18px'}}>{login1.email}</div>
+      </div>:false
+    }
+    <div className={`${style.border} ${style.height}`}>
+      <div className={`${style.flex1} ${style.height}`}>
+        <div className={style.flex2}>
+          <Route path='/about' render=
           {
-            const el=
-            <div><About/></div>
-            return el
+            ()=>
+            {
+              const el=
+              <div><About/></div>
+              return el
+            }
           }
-        }
-        />
-        <Route path='/login' render=
-        {
-          ()=><div className={`${style2.border2} ${style.fade}`}>
-              <Login {...loginState1}/>
-            </div>
-        }
-        />
-        <Route path='/' exact render=
-        {
-          ()=><div className={`${style2.border2} ${style.fade}`}><Home/></div>
-        }
-        />
+          />
+          <Route path='/login' render=
+          {
+            ()=><div className={`${style2.border2} ${style.fade}`}>
+                <Login {...loginState1}/>
+              </div>
+          }
+          />
+          <Route path='/' exact render=
+          {
+            ()=><div className={`${style2.border2} ${style.fade}`}><Home/></div>
+          }
+          />
+        </div>
       </div>
     </div>
   </div>
